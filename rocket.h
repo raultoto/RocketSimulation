a270f46f334315d05af47da09c5536983d4820ea
RaulToto <djbrexman@gmail.com>
2017-06-25 10:50:10 -0500

    lalala

--- rocket.h
+++ rocket.h
@@ -1,11 +1,14 @@
 #ifndef ROCKET_H
 #define ROCKET_H
-
-
+template <typename T>
 class Rocket
 {
 public:
-    Rocket();
+    Rocket():masa(0),altura(0){}
+    Rocket(T &m,T &a):masa(m),altura(a){}
+private:
+    T masa;
+    T altura;
 };
 
 #endif // ROCKET_H
