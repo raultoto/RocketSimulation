21d186ef2133daad01e623a5a3581e7648314def
StarUnu <etamotu@gmail.com>
2017-06-25 10:46:14 -0500

    basicasfunciones

--- rocket.h
+++ rocket.h
@@ -1,11 +1,35 @@
 #ifndef ROCKET_H
 #define ROCKET_H
+#include<cmath>
+#include<iostream>
+using namespace std ;
+template<class T>
+T radio_vector(T antheta,T V)
+{
+    cout<<"coseno"<<cos(antheta);
+    return cos(antheta)*V;
+}
 
-
+template<class T>
 class Rocket
 {
 public:
     Rocket();
+    float Posicion();
+    float Aceleracion();
+    float Velocidad();
+private:
+    float g;
+    float masacarga;
+    float densidadaire;
+    float latitud;
+    float longitud;
+    float Areatotal;
 };
+template<class T>
+float aceleracion()
+{
+
+}
 
 #endif // ROCKET_H
