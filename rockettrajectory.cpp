4220a2c60755989484e9b20c9eddd72495f8bcfc
RaulToto <djbrexman@gmail.com>
2017-07-02 20:30:04 -0500

    change trajectory color xD

--- rockettrajectory.cpp
+++ rockettrajectory.cpp
@@ -6,7 +6,7 @@ RocketTrajectory::RocketTrajectory(QGraphicsItem *parent)
     :QGraphicsTextItem(parent)
 {
     setPlainText(QString("*"));
-    setDefaultTextColor(Qt::red);
+    setDefaultTextColor(Qt::yellow);
     setFont(QFont("times",8));
-    setPos(menu->rocketInterface->x()-5,menu->rocketInterface->y());
+    setPos(menu->rocketInterface->x()-5,menu->rocketInterface->y()+8);
 }
